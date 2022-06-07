9943744f50302d77209daf14ab822034a2d9f2bf
Stuart Lacy <stuart.lacy@york.ac.uk>
2022-06-07 22:16:45 +0100

    Renamed arguments

--- detect_peaks.py
+++ detect_peaks.py
@@ -13,29 +13,29 @@ def datetime_to_seconds(dt):
 
 def detect_peaks(concentration,
                  times,
-                 std_low_threshold=0.5,    # Background std deviation
-                 plume_cutoff=3,           # Plume cutoff
-                 buffer=5,                 # Overlapping plumes
-                 rolling_period=180,       # Time to smooth background
-                 group_difference=1,       # Could probably work with 1s, shouldn't needed to be changed unless using different frequency
-                 background_window=660,    # Could try with different, could be lower
+                 bg_sd_window=180,         # Time to smooth background
+                 bg_sd_threshold=0.5,      # Background std deviation
+                 bg_mean_window=660,       # Could try with different, could be lower
                  bg_subtraction_window=10, # Trying to remove bit of noise
+                 plume_sd_threshold=3,     # Plume cutoff
+                 plume_buffer=5,           # Overlapping plumes in seconds
+                 group_difference=1,       # Could probably work with 1s, shouldn't needed to be changed unless using different frequency
                  trapz_dx=1.0):
                      
-    buffer_time = datetime.timedelta(seconds=buffer)
+    buffer_time = datetime.timedelta(seconds=plume_buffer)
     df = pd.DataFrame({'conc': concentration}, index=times)
     
     # Define background as when rolling std deviation is below a threshold
-    roll_std = df.conc.fillna(method='ffill').rolling(rolling_period, center=True).std().rolling(rolling_period, center=True).mean()
-    is_bg = (~df['conc'].isna()) & (roll_std <= std_low_threshold)
+    roll_std = df.conc.fillna(method='ffill').rolling(bg_sd_window, center=True).std().rolling(bg_sd_window, center=True).mean()
+    is_bg = (~df['conc'].isna()) & (roll_std <= bg_sd_threshold)
     
-    bg = df.conc.iloc[is_bg.values].reindex(df.index).interpolate().rolling(background_window).mean()
+    bg = df.conc.iloc[is_bg.values].reindex(df.index).interpolate().rolling(bg_mean_window).mean()
     bg_mean = df.conc.iloc[is_bg.values].mean()
     bg_std = df.conc.iloc[is_bg.values].std()
     
     # first iteration of plume detection
     conc_bg_removed = (df['conc'] - bg).rolling(bg_subtraction_window).mean()
-    is_plume = conc_bg_removed >= plume_cutoff * bg_std
+    is_plume = conc_bg_removed >= plume_sd_threshold * bg_std
     plume_groups = (is_plume != is_plume.shift()).cumsum()
     plume_groups = (
         plume_groups
